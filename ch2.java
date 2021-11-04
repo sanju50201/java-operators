ad83b9fcda87a7d640155747d3c15947bfa155f3
Sanjith Kumar V <91109178+sanju50201@users.noreply.github.com>
2021-11-04 21:16:29 +0530

    Add files via upload

---
+++ ch2.java
@@ -0,0 +1,21 @@
+public class ch2{
+    public static void main(String[] args) {
+        // Arithmetic Operators //
+        int x = 4;
+        // int y = x-2;
+        // int z = x + 2;
+        // int a = x % 3; //Modulo Operator//
+        int s = 6;
+        /* Assignment operators 
+        doesn't work for boolean 
+         '=' is an arithmetic operator */
+         s+=4;
+         System.out.println(s);
+        /* Comparison Operator '==' '>=' '<=' */
+        System.out.println(s>=8);
+        System.out.println(9>8 && 8<5); //logical operator//
+        System.out.println(9>10 || 8<5); //logical operator '&&' '||'
+        
+
+    }
+}
